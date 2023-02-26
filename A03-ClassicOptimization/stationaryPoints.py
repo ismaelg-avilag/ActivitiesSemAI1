fed099a4794349ed0927df63f7d5e8c4524e987f
Ismael Avila <ismaelg.avilag@gmail.com>
2023-02-26 14:01:41 -0600

    Se dos polinomios más para grafiicar

--- A03-ClassicOptimization/stationaryPoints.py
+++ A03-ClassicOptimization/stationaryPoints.py
@@ -7,6 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 polynomial = [1, -4, 1, 6]
+#polynomial = [6, -12, 5, -2]
+#polynomial = [1/3, 8, 63, 7]
 poly = np.poly1d(polynomial)
 first_der = np.polyder(poly, 1)
 second_der = np.polyder(poly, 1)
@@ -19,7 +21,9 @@ minior = np.min(roots) - 0.5
 x = np.linspace(minior, major, 100)
 y = np.polyval(poly, x)
 
-plt.title('Funcion: x³-4x²+x+6')
+plt.title('f(x): x³-4x²+x+6')
+#plt.title('f(x): 6x³-12x²+5x-2')
+#plt.title('f(x): (1/3)x³+8x²+63x+7')
 plt.plot(x, y, 'r')
 plt.plot(roots, values, '*g')
 plt.annotate("Minimo", (roots[0], values[0]))
