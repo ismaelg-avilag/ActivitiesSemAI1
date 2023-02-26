43ad9d0f57f16af56c08f4f13c11e5654453f4b3
Ismael Avila <ismaelg.avilag@gmail.com>
2023-02-26 13:48:52 -0600

    Se añadió la parte que convierte toma el polinomio a analizar y calcula la primera y segunda derivada. Se calculan las raices en la primera derivada, y se calculan los valores de y en la primera y segunda derivada para generar la grafica. Se calculan los rangos de x y y para la grafica y se muestran los resultados en la grafica

--- A03-ClassicOptimization/stationaryPoints.py
+++ A03-ClassicOptimization/stationaryPoints.py
@@ -3,3 +3,23 @@ Actividad: 3. Encontrar Puntos Estacionarios de Forma Automática
 Seminario de Inteligencia Artificial 1
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+polynomial = [1, -4, 1, 6]
+poly = np.poly1d(polynomial)
+first_der = np.polyder(poly, 1)
+second_der = np.polyder(poly, 1)
+roots = np.roots(first_der)
+values = np.polyval(poly, roots)
+values2d = np.polyval(second_der, roots)
+
+major = np.max(roots) + 0.5
+minior = np.min(roots) - 0.5
+x = np.linspace(minior, major, 100)
+y = np.polyval(poly, x)
+
+
+plt.plot(x, y, 'r')
+plt.plot(roots, values, '*g')
+plt.show()
