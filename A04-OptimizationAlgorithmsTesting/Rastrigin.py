6ad45234ae0b332f97d3f1bb404bb3d8cb5355ac
Ismael Avila <ismaelg.avilag@gmail.com>
2023-02-27 17:12:51 -0600

    Importación de módulos y establecimiento de valores de X y Y

--- A04-OptimizationAlgorithmsTesting/Rastrigin.py
+++ A04-OptimizationAlgorithmsTesting/Rastrigin.py
@@ -4,3 +4,16 @@ Seminario de Inteligencia Artificial 1
 
 - Rastrigin
 """
+
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+fig = plt.figure()
+ax = fig.gca(projection = '3d')
+
+X = np.arange(-5.12, 5.12, 0.1)
+Y = np.arange(-5.12, 5.12, 0.1)
+
+X, Y = np.meshgrid(X, Y)
