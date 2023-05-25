517bbdda1357051667517e2bd874797f8643aa60
Ismael Avila <ismaelg.avilag@gmail.com>
2023-05-24 20:16:12 -0600

    Algoritmo genético, problema con función de fitness y main

---
+++ 2ndExam/main.py
@@ -0,0 +1,16 @@
+"""
+Segundo Examen Parcial
+Seminario de Inteligencia Artificial 1 - D03
+
+Ismael Guadalupe Avila Guerrero - 215638591
+"""
+
+from AG import *
+from problem import *
+
+objects = [13, 8, 25, 4, 18, 6, 33, 22, 45, 11, 76, 10, 1]
+
+problem = Problem(objects, 211)
+ag = AG(13, 13, 1, 200, 0.01, problem)
+
+ag.run()
