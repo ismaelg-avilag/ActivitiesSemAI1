6f17a0bf86d4015e9cd1ada988e3a74b295a9c41
Ismael Avila <ismaelg.avilag@gmail.com>
2023-05-26 01:54:54 -0600

    Se limitó a 8 el número de clases a evaluar, para evitar duplicados con clases que duran lo mismo y evitar confusiones con los créditos correspondientes.

--- Final-Project/main.py
+++ Final-Project/main.py
@@ -8,8 +8,8 @@ Ismael Guadalupe Avila Guerrero - 215638591
 from AG import *
 from problem import *
 
-horas_clase_a_la_semana = [1, 5, 2, 6, 4, 8, 2, 7, 2, 3]
-creditos = [7, 8, 3, 1, 6, 4, 8, 3, 5, 2]
+horas_clase_a_la_semana = [1, 5, 2, 6, 4, 8, 7, 3]
+creditos = [7, 8, 3, 1, 6, 4, 8, 3]
 horas_max_semana = 25
 
 problem = Problem(horas_clase_a_la_semana, creditos, horas_max_semana)
